366ecf168b98da3f86bd40e4823f72a55d43269d
薛辛超 <xinchao.xue@bioon.com>
2022-03-10 13:53:04 +0800

    feat: 佳欢

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -2,7 +2,7 @@
   <div class="page-container">
     <div>
       <h1>404</h1>
-      <h1>OOPS！你好像走丢了...</h1>
+      <h1>OOPS！你好像走丢了.....</h1>
       <RouterLink :to="{ path: '/', replace: true }" class="ant-btn ant-btn-primary"
         >回到首页</RouterLink
       >
